0d793984a629d8abdd9bc3d2389ce25658a949fb
John Williams <johnnjit7@gmail.com>
2019-10-09 18:32:44 -0400

    Working on project now

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,10 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+var ObjectId = require("mongodb").ObjectId;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+//const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
 
 module.exports = function (app) {
 
