f46f80ec0773837cc73e55567114ce6e716fa9bb
John Williams <johnnjit7@gmail.com>
2019-10-14 19:05:12 -0400

    Working on fetching data from API

--- routes/api.js
+++ routes/api.js
@@ -14,12 +14,20 @@ var ObjectId = require("mongodb").ObjectId;
 
 //const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 const CONNECTION_STRING = "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
+const API_TOKEN = process.env.STOCK_TOKEN;
+console.log(API_TOKEN);
+const STOCK_URL='https://cloud.iexapis.com/stable/stock/';
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      let stockSticker = req.query.stock;
+      console.log('stock: ' + stockSticker);
+
+      let stockQuoteUrl = STOCK_URL + stockSticker + '/quote?token=' + API_TOKEN;
+      console.log('Url Quote: ' + stockQuoteUrl);
+
     });
     
 };
