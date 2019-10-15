7956dea6b027c322d5460d8181051125bde70e46
John Williams <johnnjit7@gmail.com>
2019-10-15 18:43:46 -0400

    Can now fetch stock data from stock api

--- routes/api.js
+++ routes/api.js
@@ -8,9 +8,12 @@
 
 'use strict';
 
+require('dotenv').config()
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require("mongodb").ObjectId;
+var fetch = require('node-fetch');
+
 
 //const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 const CONNECTION_STRING = "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
@@ -28,6 +31,20 @@ module.exports = function (app) {
       let stockQuoteUrl = STOCK_URL + stockSticker + '/quote?token=' + API_TOKEN;
       console.log('Url Quote: ' + stockQuoteUrl);
 
+      let fetchStockData = fetch(stockQuoteUrl)
+      .then(data=>{
+        return(data.json())
+      })
+      .then(result=>{
+        return({stock: result.symbol, price: result.latestPrice});
+      })
+      .catch(error=>{
+        console.log(error)
+      });
+
+      //let stockmyStockData = await fetchStockData();
+      //res.send(fetchStockData.json());
+      console.log(fetchStockData);
     });
     
 };
