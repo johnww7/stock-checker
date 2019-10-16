d2eacc24be5ab811cc5876a208cf875f80dc6371
John Williams <johnnjit7@gmail.com>
2019-10-16 18:44:42 -0400

    Still working on using fetch to retrieve data for 1 stock

--- routes/api.js
+++ routes/api.js
@@ -15,36 +15,39 @@ var ObjectId = require("mongodb").ObjectId;
 var fetch = require('node-fetch');
 
 
-//const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-const CONNECTION_STRING = "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
+const CONNECTION_STRING = process.env.CONNECTION_STRING; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+//const CONNECTION_STRING = "mongodb://john:N1teLockon@ds035787.mlab.com:35787/jwfccmongodb";
 const API_TOKEN = process.env.STOCK_TOKEN;
-console.log(API_TOKEN);
+//console.log(API_TOKEN);
 const STOCK_URL='https://cloud.iexapis.com/stable/stock/';
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      let stockSticker = req.query.stock;
-      console.log('stock: ' + stockSticker);
+      let stockSticker = req.query;
+      let stockData;
+      console.log('stock: ' + JSON.stringify(stockSticker) + 'type: ' + typeof(stockSticker));
 
       let stockQuoteUrl = STOCK_URL + stockSticker + '/quote?token=' + API_TOKEN;
       console.log('Url Quote: ' + stockQuoteUrl);
 
-      let fetchStockData = fetch(stockQuoteUrl)
-      .then(data=>{
+      let fetchStockData = fetch(stockQuoteUrl);
+      fetchStockData.then(data=>{
         return(data.json())
       })
       .then(result=>{
-        return({stock: result.symbol, price: result.latestPrice});
+        stockData = ({stock: result.symbol, price: result.latestPrice});
+        console.log('Data: ' + JSON.stringify(stockData));
       })
       .catch(error=>{
         console.log(error)
       });
 
+
       //let stockmyStockData = await fetchStockData();
       //res.send(fetchStockData.json());
-      console.log(fetchStockData);
+      console.log('Data: ' + JSON.stringify(stockData));
     });
     
 };
