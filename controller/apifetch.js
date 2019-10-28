8c3d3dd292f6deaf0a097213cb83008624f61518
John Williams <johnnjit7@gmail.com>
2019-10-28 19:55:45 -0400

    Created two new files for mongo queries and fetching api data

---
+++ controller/apifetch.js
@@ -0,0 +1,10 @@
+var fetch = require('node-fetch');
+const API_TOKEN = process.env.STOCK_TOKEN;
+//console.log(API_TOKEN);
+const STOCK_URL='https://cloud.iexapis.com/stable/stock/';
+
+aysnc function fetchStockData(quoteUrl) {
+
+}
+
+module.exports.fetchStockData = this.fetchStockData;
